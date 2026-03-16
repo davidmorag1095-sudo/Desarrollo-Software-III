7141814e8bec23ba10d4e16ae954141d3337f0ae
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-03-16 17:58:58 -0600

    sdas

--- David/Semana 2/Sistema_Flete/Controller/Controller.py
+++ David/Semana 2/Sistema_Flete/Controller/Controller.py
@@ -1,7 +1,7 @@
 #Importaciones para poder hacer instancias de las clases
-from Semana1.Sistema_Flete.Model.RepositorioModel import Repositorio
-from Semana1.Sistema_Flete.Model.ClienteModel import Cliente
-from Semana1.Sistema_Flete.Model.FleteModel import Flete
+from Semana2.Sistema_Flete.Model.RepositorioModel import Repositorio
+from Semana2.Sistema_Flete.Model.ClienteModel import Cliente
+from Semana2.Sistema_Flete.Model.FleteModel import Flete
 class Controller:
     # -----------------------------------------------------------------------
     #Constructor
