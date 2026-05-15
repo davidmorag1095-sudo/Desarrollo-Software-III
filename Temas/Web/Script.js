6644e7fcbc4a660271b57053affcabdb8c0c7b9e
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-05-14 18:31:42 -0600

    asd

---
+++ Temas/Web/Script.js
@@ -0,0 +1,8 @@
+function cambiarTexto() {
+
+    const texto = document.getElementById("texto");
+
+    texto.innerHTML = "¡Gracias por hacer clic en el botón!";
+
+    alert("Proximamente habra mas!");
+}
