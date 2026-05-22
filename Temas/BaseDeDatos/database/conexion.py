d6883af9b2b5706c8ab292c2a550e24881e2361f
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-05-21 20:29:29 -0600

    asd

---
+++ Temas/BaseDeDatos/database/conexion.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def obtener_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="universidad",
+            port=3306,
+        )
+        return conexion
+    except Error as error:
+        print("Error al conectar la base de datos")
+
+        raise
