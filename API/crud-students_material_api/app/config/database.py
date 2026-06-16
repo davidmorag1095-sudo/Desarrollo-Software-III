7506bd5ac4066297937a22174ecd19fc05c1048f
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-06-15 20:02:56 -0600

    sdf

--- API/crud-students_material_api/app/config/database.py
+++ API/crud-students_material_api/app/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/students_db"
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/students_db"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
--- API/crud-students_material_api/app/controller/student_controller_api.py
+++ API/crud-students_material_api/app/controller/student_controller_api.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.testing.provision import update_db_opts
 from starlette import status
 
 from app.schemas.student_schema import StudentSchema
@@ -10,3 +11,28 @@ service = StudentService()
 @router.post("/", response_model=StudentSchema)
 def create_student(student: StudentSchema):
     return service.create_student(student.carnet, student.name, student.age)
+
+@router.get("/{carnet}", response_model=StudentSchema)
+def get_student(carnet: str):
+    student = service.get_student(carnet)
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+
+@router.get("/", response_model=list[StudentSchema])
+def list_students():
+    return service.list_students()
+
+@router.put("/{carnet}", response_model=StudentSchema)
+def update_student(carnet: str, student: StudentSchema):
+    updated = service.update_student(carnet, student.name, student.age)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+
+@router.delete("/{carnet}")
+def delete_student(carnet: str):
+    deleted = service.delete_student(carnet)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return {"Message:" "Student deleted"}
